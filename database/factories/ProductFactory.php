0624bfb86b5bf0a0eacec86397948589d864337f
mohammad <mohammadkh12221@gmail.com>
2024-07-07 15:48:14 +0330

    order widgets

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Brand;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,21 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            //
-        ];
+            "category_id" => Category::inRandomOrder()->first()->id,
+            "brand_id" => Brand::inRandomOrder()->first()->id,
+            "name" => collect(["apple laptop2021" , "asus 2020" , "dell 2021" ])->random(),
+            "slug" => fake()->slug,
+            'images' => json_encode([
+                'url1' => $this->faker->imageUrl(),
+                'url2' => $this->faker->imageUrl(),
+                'url3' => $this->faker->imageUrl(),
+            ]),
+            "description" => $this->faker->sentence(),
+            'price' => $this->faker->randomFloat(2, 0, 9999.99),
+            'is_active' => $this->faker->boolean,
+            'is_featured' => $this->faker->boolean,
+            'in_stock' => $this->faker->boolean,
+            'on_sale' => $this->faker->boolean,
+            ];
     }
 }
