a033f5c6a6afb2dc94fbca008ea5554fcba42242
mohammad <mohammadkh12221@gmail.com>
2024-07-07 17:41:19 +0330

    Ui Pages

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/' , \App\Livewire\HomePage::class);
+Route::get('/categories' , \App\Livewire\CategoriesPage::class);
+Route::get('/products' , \App\Livewire\ProductsPage::class);
+Route::get('/products/{product}' , \App\Livewire\ProductDetailPage::class);
+Route::get('/cart' , \App\Livewire\CartPage::class);
+Route::get('/checkout' , \App\Livewire\CheckoutPage::class);
+Route::get('/my-orders' , \App\Livewire\MyOrdersPage::class);
+Route::get('/my-orders/{order}' , \App\Livewire\MyOrderDetailPage::class);
+
+Route::get('/login' , \App\Livewire\Auth\Login::class);
+Route::get('/register' , \App\Livewire\Auth\Register::class);
+Route::get('/forget-password' , \App\Livewire\Auth\ForgetPassword::class);
+Route::get('/reset-password' , \App\Livewire\Auth\ResetPassword::class);
+
+Route::get("/success" , \App\Livewire\SuccessPage::class);
+Route::get("/cancel" , \App\Livewire\CancelPage::class);
+
