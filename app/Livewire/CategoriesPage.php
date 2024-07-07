ca174c86ac440cc363c99d7856a55b62d7b88c10
mohammad <mohammadkh12221@gmail.com>
2024-07-07 18:11:07 +0330

    data in home page'

--- app/Livewire/CategoriesPage.php
+++ app/Livewire/CategoriesPage.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\Category;
+use Livewire\Attributes\Title;
 use Livewire\Component;
-
+#[Title("Category Page")]
 class CategoriesPage extends Component
 {
     public function render()
     {
-        return view('livewire.categories-page');
+        $categories = Category::where("is_active" , 1)->get();
+        return view('livewire.categories-page')->with([
+            "categories" => $categories
+        ]);
     }
 }
