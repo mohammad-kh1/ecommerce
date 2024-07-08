66d54a5d13612b1ce0b1ba8b1ec11d5755a225a1
mohammad <mohammadkh12221@gmail.com>
2024-07-08 11:39:01 +0330

    login and register

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -2,10 +2,27 @@
 
 namespace App\Livewire\Auth;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Login extends Component
 {
+    #[Validate("required|email")]
+    public string $email;
+    #[Validate("required")]
+    public string $password;
+
+    public function save()
+    {
+        $this->validate();
+
+        if(!auth()->attempt(["email" => $this->email , "password" => $this->password])){
+            session()->flash("error","invalid credentials");
+            return;
+        }
+
+        return redirect()->intended();
+    }
     public function render()
     {
         return view('livewire.auth.login');
--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -2,10 +2,34 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
+#[Title("register")]
 class Register extends Component
 {
+
+    #[Validate("required|max:100|min:3")]
+    public string $name;
+    #[Validate("required|unique:users")]
+    public string $email;
+    #[Validate("required|min:6|max:8")]
+    public string $password;
+
+    public function save()
+    {
+        $this->validate();
+        $user = User::create([
+            "name" =>$this->name,
+            "email" =>$this->email,
+            "password" =>bcrypt($this->password)
+        ]);
+        //login
+        auth()->login($user);
+        return redirect()->intended();
+    }
     public function render()
     {
         return view('livewire.auth.register');
